baecabb344a78db20892f4696fd25b3464cf0c27
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-07 08:24:10 +0200

    Applying Code Cleanup.

--- CK.DB.Res.ResPath/ResPathTable.cs
+++ CK.DB.Res.ResPath/ResPathTable.cs
@@ -2,97 +2,96 @@ using CK.Core;
 using CK.SqlServer;
 using System.Threading.Tasks;
 
-namespace CK.DB.Res.ResPath
+namespace CK.DB.Res.ResPath;
+
+/// <summary>
+/// This table holds the resource name.
+/// Resource names implement a path-based hierarchy.
+/// </summary>
+[SqlTable( "tResPath", Package = typeof( Package ) )]
+[Versions( "1.0.0" )]
+[SqlObjectItem( "transform:vRes, transform:sResDestroy" )]
+[SqlObjectItem( "fResPathPrefixes" )]
+[SqlObjectItem( "vResPathAllChildren, vResPathDirectChildren, vResPathParentPrefixes" )]
+public abstract partial class ResPathTable : SqlTable
 {
     /// <summary>
-    /// This table holds the resource name.
-    /// Resource names implement a path-based hierarchy.
+    /// Renames a resource, by default renaming also its children.
+    /// Nothing is done if the resource does not exist or has no associated ResPath.
     /// </summary>
-    [SqlTable( "tResPath", Package = typeof( Package ) )]
-    [Versions( "1.0.0" )]
-    [SqlObjectItem( "transform:vRes, transform:sResDestroy" )]
-    [SqlObjectItem( "fResPathPrefixes" )]
-    [SqlObjectItem( "vResPathAllChildren, vResPathDirectChildren, vResPathParentPrefixes" )]
-    public abstract partial class ResPathTable : SqlTable
-    {
-        /// <summary>
-        /// Renames a resource, by default renaming also its children.
-        /// Nothing is done if the resource does not exist or has no associated ResPath.
-        /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="resId">The resource identifier to rename.</param>
-        /// <param name="newName">The new resource name.</param>
-        /// <param name="withChildren">
-        /// False to rename only this resource and not its children: children
-        /// names are left as-is as "orphans".
-        /// </param>
-        /// <returns>The awaitable.</returns>
-        [SqlProcedure( "sResPathRename" )]
-        public abstract Task RenameAsync( ISqlCallContext ctx, int resId, string newName, bool withChildren = true );
+    /// <param name="ctx">The call context.</param>
+    /// <param name="resId">The resource identifier to rename.</param>
+    /// <param name="newName">The new resource name.</param>
+    /// <param name="withChildren">
+    /// False to rename only this resource and not its children: children
+    /// names are left as-is as "orphans".
+    /// </param>
+    /// <returns>The awaitable.</returns>
+    [SqlProcedure( "sResPathRename" )]
+    public abstract Task RenameAsync( ISqlCallContext ctx, int resId, string newName, bool withChildren = true );
 
-        /// <summary>
-        /// Renames a resource, by default renaming also its children.
-        /// Nothing is done if the resource does not exist or has no associated ResPath.
-        /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="oldName">The resource name to rename.</param>
-        /// <param name="newName">The new resource name.</param>
-        /// <param name="withChildren">
-        /// False to rename only this resource and not its children: children
-        /// names are left as-is as "orphans".
-        /// </param>
-        /// <returns>The awaitable.</returns>
-        [SqlProcedure( "sResPathRenameResPath" )]
-        public abstract Task RenameAsync( ISqlCallContext ctx, string oldName, string newName, bool withChildren = true );
+    /// <summary>
+    /// Renames a resource, by default renaming also its children.
+    /// Nothing is done if the resource does not exist or has no associated ResPath.
+    /// </summary>
+    /// <param name="ctx">The call context.</param>
+    /// <param name="oldName">The resource name to rename.</param>
+    /// <param name="newName">The new resource name.</param>
+    /// <param name="withChildren">
+    /// False to rename only this resource and not its children: children
+    /// names are left as-is as "orphans".
+    /// </param>
+    /// <returns>The awaitable.</returns>
+    [SqlProcedure( "sResPathRenameResPath" )]
+    public abstract Task RenameAsync( ISqlCallContext ctx, string oldName, string newName, bool withChildren = true );
 
-        /// <summary>
-        /// Creates a new resource name for an existing resource identifier.
-        /// There must not be an existing resource name.
-        /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="resId">The resource identifier that must exist.</param>
-        /// <param name="ResPath">The resource name to create.</param>
-        /// <returns>The awaitable.</returns>
-        [SqlProcedure( "sResPathCreate" )]
-        public abstract Task CreateResPathAsync( ISqlCallContext ctx, int resId, string ResPath );
+    /// <summary>
+    /// Creates a new resource name for an existing resource identifier.
+    /// There must not be an existing resource name.
+    /// </summary>
+    /// <param name="ctx">The call context.</param>
+    /// <param name="resId">The resource identifier that must exist.</param>
+    /// <param name="ResPath">The resource name to create.</param>
+    /// <returns>The awaitable.</returns>
+    [SqlProcedure( "sResPathCreate" )]
+    public abstract Task CreateResPathAsync( ISqlCallContext ctx, int resId, string ResPath );
 
-        /// <summary>
-        /// Creates a new resource with a name.
-        /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="ResPath">The resource name to create.</param>
-        /// <returns>The new resource identifier.</returns>
-        [SqlProcedure( "sResCreateWithResPath" )]
-        public abstract Task<int> CreateWithResPathAsync( ISqlCallContext ctx, string ResPath );
+    /// <summary>
+    /// Creates a new resource with a name.
+    /// </summary>
+    /// <param name="ctx">The call context.</param>
+    /// <param name="ResPath">The resource name to create.</param>
+    /// <returns>The new resource identifier.</returns>
+    [SqlProcedure( "sResCreateWithResPath" )]
+    public abstract Task<int> CreateWithResPathAsync( ISqlCallContext ctx, string ResPath );
 
-        /// <summary>
-        /// Destroys the resource name associated to a resource identifier if any.
-        /// This doesn't destroy the resource itself, only the name.
-        /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="resId">The resoource identifier.</param>
-        /// <returns>The awaitable.</returns>
-        [SqlProcedure( "sResPathDestroy" )]
-        public abstract Task DestroyResPathAsync( ISqlCallContext ctx, int resId );
+    /// <summary>
+    /// Destroys the resource name associated to a resource identifier if any.
+    /// This doesn't destroy the resource itself, only the name.
+    /// </summary>
+    /// <param name="ctx">The call context.</param>
+    /// <param name="resId">The resoource identifier.</param>
+    /// <returns>The awaitable.</returns>
+    [SqlProcedure( "sResPathDestroy" )]
+    public abstract Task DestroyResPathAsync( ISqlCallContext ctx, int resId );
 
-        /// <summary>
-        /// Destroys a root resource and/or its children thanks to its name.
-        /// Note that if <paramref name="withRoot"/> and <paramref name="withChildren"/> are both false, nothing is done.
-        /// If the root name doesn't exist, its children can nevertheless be destroyed.
-        /// Setting <paramref name="ResPathOnly"/> to false will call CK.sResDestroy, destroying the ResId 
-        /// and all its resources. By default, only the resource name is destroyed (this is the safest way).
-        /// /// </summary>
-        /// <param name="ctx">The call context.</param>
-        /// <param name="rootResPath">The root resource name to destroy.</param>
-        /// <param name="withRoot">Whether the root itself must be destroyed.</param>
-        /// <param name="withChildren">Whether the root's children must be destroyed.</param>
-        /// <param name="ResPathOnly">
-        /// Set it it false to call sResDestroy (destroying the whole resources) instead of sResPathDestroy.
-        /// This has be set explicitely.
-        /// </param>
-        /// <returns>The awaitable.</returns>
-        [SqlProcedure( "sResDestroyByResPath" )]
-        public abstract Task DestroyByResPathAsync( ISqlCallContext ctx, string rootResPath, bool withRoot = true, bool withChildren = true, bool ResPathOnly = true );
+    /// <summary>
+    /// Destroys a root resource and/or its children thanks to its name.
+    /// Note that if <paramref name="withRoot"/> and <paramref name="withChildren"/> are both false, nothing is done.
+    /// If the root name doesn't exist, its children can nevertheless be destroyed.
+    /// Setting <paramref name="ResPathOnly"/> to false will call CK.sResDestroy, destroying the ResId 
+    /// and all its resources. By default, only the resource name is destroyed (this is the safest way).
+    /// /// </summary>
+    /// <param name="ctx">The call context.</param>
+    /// <param name="rootResPath">The root resource name to destroy.</param>
+    /// <param name="withRoot">Whether the root itself must be destroyed.</param>
+    /// <param name="withChildren">Whether the root's children must be destroyed.</param>
+    /// <param name="ResPathOnly">
+    /// Set it it false to call sResDestroy (destroying the whole resources) instead of sResPathDestroy.
+    /// This has be set explicitely.
+    /// </param>
+    /// <returns>The awaitable.</returns>
+    [SqlProcedure( "sResDestroyByResPath" )]
+    public abstract Task DestroyByResPathAsync( ISqlCallContext ctx, string rootResPath, bool withRoot = true, bool withChildren = true, bool ResPathOnly = true );
 
-    }
 }
--- Tests/CK.DB.Res.ResPath.Tests/ResPathTests.cs
+++ Tests/CK.DB.Res.ResPath.Tests/ResPathTests.cs
@@ -6,159 +6,158 @@ using NUnit.Framework;
 using System;
 using static CK.Testing.MonitorTestHelper;
 
-namespace CK.DB.Res.ResPath.Tests
+namespace CK.DB.Res.ResPath.Tests;
+
+[TestFixture]
+public class ResPathTests
 {
-    [TestFixture]
-    public class ResPathTests
+    [Test]
+    public void resource_0_and_1_are_empty_and_System()
     {
-        [Test]
-        public void resource_0_and_1_are_empty_and_System()
+        var r = SharedEngine.Map.StObjs.Obtain<ResPathTable>();
+        r.Database.ExecuteScalar( "select ResPath from CK.vRes where ResId = 0" )
+            .Should().Be( "" );
+        r.Database.ExecuteScalar( "select ResPath from CK.vRes where ResId = 1" )
+            .Should().Be( "System" );
+    }
+
+
+    [Test]
+    public void resource_0_and_1_can_not_be_destroyed()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var r = SharedEngine.Map.StObjs.Obtain<ResPathTable>();
-            r.Database.ExecuteScalar( "select ResPath from CK.vRes where ResId = 0" )
-                .Should().Be( "" );
-            r.Database.ExecuteScalar( "select ResPath from CK.vRes where ResId = 1" )
-                .Should().Be( "System" );
+            p.Invoking( sut => sut.ResTable.Destroy( ctx, 0 ) ).Should().Throw<SqlDetailedException>();
+            p.Invoking( sut => sut.ResTable.Destroy( ctx, 1 ) ).Should().Throw<SqlDetailedException>();
         }
+    }
 
-
-        [Test]
-        public void resource_0_and_1_can_not_be_destroyed()
+    [Test]
+    public void CreateResPath_raises_an_exception_if_the_resource_is_already_associated_to_a_path_or_the_path_already_exists()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                p.Invoking( sut => sut.ResTable.Destroy( ctx, 0 ) ).Should().Throw<SqlDetailedException>();
-                p.Invoking( sut => sut.ResTable.Destroy( ctx, 1 ) ).Should().Throw<SqlDetailedException>();
-            }
+            int resId = p.ResTable.Create( ctx );
+            string ResPath = Guid.NewGuid().ToString();
+            string ResPath2 = Guid.NewGuid().ToString();
+            p.ResPathTable.CreateResPath( ctx, resId, ResPath );
+            // Creates where a name already exists.
+            p.Invoking( sut => sut.ResPathTable.CreateResPath( ctx, resId, ResPath2 ) ).Should().Throw<SqlDetailedException>();
+            // Creates with an already existing name.
+            int resId2 = p.ResTable.Create( ctx );
+            p.Invoking( sut => sut.ResPathTable.CreateResPath( ctx, resId2, ResPath ) ).Should().Throw<SqlDetailedException>();
+            p.ResTable.Destroy( ctx, resId );
+            p.ResTable.Destroy( ctx, resId2 );
         }
+    }
 
-        [Test]
-        public void CreateResPath_raises_an_exception_if_the_resource_is_already_associated_to_a_path_or_the_path_already_exists()
+    [Test]
+    public void renaming_a_resource_can_be_done_WithChildren_or_only_for_the_resource_itself_by_resId()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                int resId = p.ResTable.Create( ctx );
-                string ResPath = Guid.NewGuid().ToString();
-                string ResPath2 = Guid.NewGuid().ToString();
-                p.ResPathTable.CreateResPath( ctx, resId, ResPath );
-                // Creates where a name already exists.
-                p.Invoking( sut => sut.ResPathTable.CreateResPath( ctx, resId, ResPath2 ) ).Should().Throw<SqlDetailedException>();
-                // Creates with an already existing name.
-                int resId2 = p.ResTable.Create( ctx );
-                p.Invoking( sut => sut.ResPathTable.CreateResPath( ctx, resId2, ResPath ) ).Should().Throw<SqlDetailedException>();
-                p.ResTable.Destroy( ctx, resId );
-                p.ResTable.Destroy( ctx, resId2 );
-            }
-        }
+            p.ResPathTable.DestroyByResPath( ctx, "Test", ResPathOnly: false );
 
-        [Test]
-        public void renaming_a_resource_can_be_done_WithChildren_or_only_for_the_resource_itself_by_resId()
-        {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                p.ResPathTable.DestroyByResPath( ctx, "Test", ResPathOnly: false );
+            int n1 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root" );
+            int n2 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1" );
+            int n3 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1/1" );
 
-                int n1 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root" );
-                int n2 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1" );
-                int n3 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1/1" );
+            p.ResPathTable.Rename( ctx, n1, "Test/-Root-" );
+            p.Database.ExecuteReader( "select * from CK.tResPath where ResPath like 'Test/Root%'" )
+                .Rows.Should().BeEmpty();
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-'" )
+                .Should().Be( n1 );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
+                .Should().Be( n2 );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
+                .Should().Be( n3 );
 
-                p.ResPathTable.Rename( ctx, n1, "Test/-Root-" );
-                p.Database.ExecuteReader( "select * from CK.tResPath where ResPath like 'Test/Root%'" )
-                    .Rows.Should().BeEmpty();
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-'" )
+            p.ResPathTable.Rename( ctx, n1, "Test/MovedTheRootOnly", withChildren: false );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/MovedTheRootOnly'" )
                     .Should().Be( n1 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
                     .Should().Be( n2 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
-                    .Should().Be( n3 );
-
-                p.ResPathTable.Rename( ctx, n1, "Test/MovedTheRootOnly", withChildren: false );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/MovedTheRootOnly'" )
-                        .Should().Be( n1 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
-                        .Should().Be( n2 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
-                    .Should().Be( n3 );
-            }
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
+                .Should().Be( n3 );
         }
+    }
 
-        [Test]
-        public void renaming_a_resource_can_be_done_WithChildren_or_only_for_the_resource_itself_by_ResPath()
+    [Test]
+    public void renaming_a_resource_can_be_done_WithChildren_or_only_for_the_resource_itself_by_ResPath()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                p.ResPathTable.DestroyByResPath( ctx, "Test", ResPathOnly: false );
+            p.ResPathTable.DestroyByResPath( ctx, "Test", ResPathOnly: false );
 
-                int n1 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root" );
-                int n2 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1" );
-                int n3 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1/1" );
+            int n1 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root" );
+            int n2 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1" );
+            int n3 = p.ResPathTable.CreateWithResPath( ctx, "Test/Root/1/1" );
 
-                p.ResPathTable.Rename( ctx, "Test/Root", "Test/-Root-" );
-                p.Database.ExecuteReader( "select * from CK.tResPath where ResPath like 'Test/Root%'" )
-                    .Rows.Should().BeEmpty();
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-'" )
+            p.ResPathTable.Rename( ctx, "Test/Root", "Test/-Root-" );
+            p.Database.ExecuteReader( "select * from CK.tResPath where ResPath like 'Test/Root%'" )
+                .Rows.Should().BeEmpty();
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-'" )
+                .Should().Be( n1 );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
+                .Should().Be( n2 );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
+                .Should().Be( n3 );
+
+            p.ResPathTable.Rename( ctx, "Test/-Root-", "Test/MovedTheRootOnly", withChildren: false );
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/MovedTheRootOnly'" )
                     .Should().Be( n1 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
                     .Should().Be( n2 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
-                    .Should().Be( n3 );
-
-                p.ResPathTable.Rename( ctx, "Test/-Root-", "Test/MovedTheRootOnly", withChildren: false );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/MovedTheRootOnly'" )
-                        .Should().Be( n1 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1'" )
-                        .Should().Be( n2 );
-                p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
-                    .Should().Be( n3 );
-            }
+            p.Database.ExecuteScalar( "select ResId from CK.tResPath where ResPath='Test/-Root-/1/1'" )
+                .Should().Be( n3 );
         }
+    }
 
-        [Test]
-        public void using_DestroyByPrefix_enables_destruction_without_an_existing_parent()
+    [Test]
+    public void using_DestroyByPrefix_enables_destruction_without_an_existing_parent()
+    {
+        var p = SharedEngine.Map.StObjs.Obtain<Package>();
+        using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
         {
-            var p = SharedEngine.Map.StObjs.Obtain<Package>();
-            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
-            {
-                var nameRoot = Guid.NewGuid().ToString();
+            var nameRoot = Guid.NewGuid().ToString();
 
-                int n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
-                int n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root.1" );
-                int n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
+            int n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
+            int n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root.1" );
+            int n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
 
-                p.ResPathTable.DestroyByResPath( ctx, nameRoot, withRoot: true, withChildren: true, ResPathOnly: false );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 0 );
-                p.Database.ExecuteScalar( "select count(*) from CK.tRes where ResId in (@0, @1, @2)", n1, n2, n3 ).Should().Be( 0 );
+            p.ResPathTable.DestroyByResPath( ctx, nameRoot, withRoot: true, withChildren: true, ResPathOnly: false );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 0 );
+            p.Database.ExecuteScalar( "select count(*) from CK.tRes where ResId in (@0, @1, @2)", n1, n2, n3 ).Should().Be( 0 );
 
-                // Destroys root, keep children.
-                n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
-                n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1" );
-                n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
+            // Destroys root, keep children.
+            n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
+            n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1" );
+            n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
 
-                p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: false, ResPathOnly: false );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 2 );
+            p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: false, ResPathOnly: false );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 2 );
 
-                p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: true, ResPathOnly: false );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 0 );
+            p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: true, ResPathOnly: false );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 0 );
 
-                // Destroys children but keep the root.
-                n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
-                n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1" );
-                n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
+            // Destroys children but keep the root.
+            n1 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root" );
+            n2 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1" );
+            n3 = p.ResPathTable.CreateWithResPath( ctx, nameRoot + "/Test/Root/1/1" );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0+'%'", nameRoot ).Should().Be( 3 );
 
-                p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: false, withChildren: true, ResPathOnly: false );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 1 );
+            p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: false, withChildren: true, ResPathOnly: false );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 1 );
 
-                p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: false, ResPathOnly: false );
-                p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 0 );
+            p.ResPathTable.DestroyByResPath( ctx, nameRoot + "/Test/Root", withRoot: true, withChildren: false, ResPathOnly: false );
+            p.Database.ExecuteScalar( "select count(*) from CK.tResPath where ResPath like @0 + '%'", nameRoot ).Should().Be( 0 );
 
-            }
         }
     }
 }
